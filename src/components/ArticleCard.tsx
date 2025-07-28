c8ebd5ed97d23a3401a6b9e7bddb26a38592b7d9
Muhammad Khattab <msskzx@gmail.com>
2025-07-28 20:40:07 +0200

    fix: img width

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -18,7 +18,7 @@ function ArticleCard({ article }: { article: ArticleBase }) {
       <div className="bg-gray-50 dark:bg-gray-900 border-l-4 border-amber-400 rounded-lg p-6 shadow transition-transform duration-200 hover:shadow-xl hover:scale-105 hover:bg-gray-50 dark:hover:bg-gray-700 cursor-pointer h-full flex flex-col">
         {article.img && (
           <div className="overflow-hidden rounded-lg mb-4">
-            <Image src={article.img} alt={article.title} className="w-full h-auto object-cover" />
+            <Image src={article.img} alt={article.title} width={500} height={500} className="object-cover" />
           </div>
         )} 
         {article.special && (
